a3bb9b0c6508fd102dc17754a448f1f667fd83e6
no0tno0t <daniel_j.strand@hotmail.com>
2021-11-10 04:33:52 +0000

    add function for random food placement in food.js

--- assets/js/food.js
+++ assets/js/food.js
@@ -1,12 +1,14 @@
 import { onSnake, expandSnake } from './snake.js'
 
-let food = { x: 10, y: 5}
+
+
+let food = RandomFood()
 const EXPANSION_RATE = 1
 
 export function update() {
     if (onSnake(food)) {
       expandSnake(EXPANSION_RATE)
-      food = { x: 5, y: 10 }
+      food = RandomFood()
     }
   }
   
@@ -17,3 +19,13 @@ export function update() {
     foodElement.classList.add("food")
     gameBoard.appendChild(foodElement)
 }
+
+
+
+function RandomFood() {
+    let newFood
+    while (newFood == null || onSnake(newFood)) {
+      newFood = randomGridPosition()
+    }
+    return newFood
+  }
--- assets/js/snake.js
+++ assets/js/snake.js
@@ -5,8 +5,7 @@ const snakeBody = [{ x: 11, y: 11 }]
 let newSegments = 0
 
 export function update() {
-  
-
+    addSegments()
     const inputDirection = getInputDirection()
     for (let i = snakeBody.length - 2; i >= 0; i--) {
       snakeBody[i + 1] = { ...snakeBody[i] }
